358f6d6406e191a24bad52a11fc8a9da84fe3bdc
ScriptSpectra <scriptspectra@gmail.com>
2026-04-17 11:19:30 +0530

    fixing clerk webhook issues #2

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -98,7 +98,7 @@ export async function POST(req: Request) {
                     where: {
                         OR: [{ clerkUserId }, { email }],
                     },
-                    update: {
+                    data: {
                         clerkUserId,
                         email,
                     },
